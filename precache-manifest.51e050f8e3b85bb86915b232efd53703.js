6011385d8059840df425be0fae8a009fb6738572
mikemikemikemikemmmm <36297097+mikemikemikemikemmmm@users.noreply.github.com>
2020-01-17 23:31:27 +0800

    first commit
    first commit

---
+++ precache-manifest.51e050f8e3b85bb86915b232efd53703.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "601ca9b4a41a41decd1361a6eb5b7a74",
+    "url": "/almanacDemo/index.html"
+  },
+  {
+    "revision": "42935f912c98af003ab7",
+    "url": "/almanacDemo/static/js/2.7bcbfe58.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/almanacDemo/static/js/2.7bcbfe58.chunk.js.LICENSE"
+  },
+  {
+    "revision": "a826ebac6b95709aed7b",
+    "url": "/almanacDemo/static/js/main.9c2bbaf1.chunk.js"
+  },
+  {
+    "revision": "2ff7d03690c4c4db636d",
+    "url": "/almanacDemo/static/js/runtime-main.2b859e08.js"
+  }
+]);
